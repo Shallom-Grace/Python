e876fa877b713aea9035d358c7b3d9a20dc85649
Grace Shallom <114561498+Shallom-Grace@users.noreply.github.com>
2025-01-30 13:33:52 +0300

    Update votingTwo.py

--- votingTwo.py
+++ votingTwo.py
@@ -1,11 +1,16 @@
 try: 
+    #get user's age and citizenship
     age = int(input("Enter your age: "))
+    #convert case to lower for case-sensitive comparison
     citizen = input("Choose the country you are a citizen of: (Kenya, Tanzania, Uganda) :").lower()
+    #defining the list of valid countries
     valid_countries = ["kenya", "tanzania", "uganda"]
 
+    #checking if the user meets both the age and citizenship requirements
     if (age >= 18 and citizen in valid_countries):
         print(f"Eligible to vote!! country{citizen} and age{age}")
     else:
         print("Not eligible to vote!!")
+#handles non-numeric input for age
 except ValueError:
     print("Enter Numeric for age!")
